7139cdfbe01d9437dcf60b8fca6b417eab758187
Renan Vasconelos <renan.vasconcelos@liferay.com>
2023-04-20 14:18:21 -0300

    document-library-taglib Change DLViewFileVersionDisplayContextUtil to a non-component class by using snapshot

--- modules/apps/document-library/document-library-taglib/src/main/java/com/liferay/document/library/taglib/internal/display/context/DLViewFileVersionDisplayContextUtil.java
+++ modules/apps/document-library/document-library-taglib/src/main/java/com/liferay/document/library/taglib/internal/display/context/DLViewFileVersionDisplayContextUtil.java
@@ -16,6 +16,7 @@ package com.liferay.document.library.taglib.internal.display.context;
 
 import com.liferay.document.library.display.context.DLDisplayContextProvider;
 import com.liferay.document.library.display.context.DLViewFileVersionDisplayContext;
+import com.liferay.osgi.util.service.Snapshot;
 import com.liferay.portal.kernel.repository.model.FileVersion;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,12 +26,10 @@ import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alejandro Tardín
  */
-@Component(service = {})
 public class DLViewFileVersionDisplayContextUtil {
 
 	public static DLViewFileVersionDisplayContext
@@ -38,25 +37,14 @@ public class DLViewFileVersionDisplayContextUtil {
 			HttpServletRequest httpServletRequest,
 			HttpServletResponse httpServletResponse, FileVersion fileVersion) {
 
-		return _dlViewFileVersionDisplayContextUtil._dlDisplayContextProvider.
+		return _dlDisplayContextProviderSnapshot.get().
 			getDLViewFileVersionDisplayContext(
 				httpServletRequest, httpServletResponse, fileVersion);
 	}
 
-	@Activate
-	protected void activate(BundleContext bundleContext) {
-		_dlViewFileVersionDisplayContextUtil = this;
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_dlViewFileVersionDisplayContextUtil = null;
-	}
-
-	private static DLViewFileVersionDisplayContextUtil
-		_dlViewFileVersionDisplayContextUtil;
-
-	@Reference
-	private DLDisplayContextProvider _dlDisplayContextProvider;
+	private static final Snapshot<DLDisplayContextProvider>
+		_dlDisplayContextProviderSnapshot = new Snapshot<>(
+		DLViewFileVersionDisplayContextUtil.class, DLDisplayContextProvider.class
+	);
 
 }
