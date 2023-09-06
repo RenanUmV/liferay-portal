9179f3c19da0c300c019351f3342a5d7da697422
Renan Vasconelos <renan.vasconcelos@liferay.com>
2023-09-06 17:32:13 -0300

    LPS-195490 Make _updateLogLevels method static to avoid implementing IdentifiableOSGiService

--- modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/portlet/action/EditServerMVCActionCommand.java
+++ modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/portlet/action/EditServerMVCActionCommand.java
@@ -53,8 +53,6 @@ import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.ResourcePermission;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
-import com.liferay.portal.kernel.module.framework.service.IdentifiableOSGiService;
-import com.liferay.portal.kernel.module.framework.service.IdentifiableOSGiServiceUtil;
 import com.liferay.portal.kernel.portlet.LiferayActionResponse;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
@@ -140,10 +138,9 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.name=" + PortletKeys.SERVER_ADMIN,
 		"mvc.command.name=/server_admin/edit_server"
 	},
-	service = {IdentifiableOSGiService.class, MVCActionCommand.class}
+	service = MVCActionCommand.class
 )
-public class EditServerMVCActionCommand
-	extends BaseMVCActionCommand implements IdentifiableOSGiService {
+public class EditServerMVCActionCommand extends BaseMVCActionCommand {
 
 	@Override
 	public void doProcessAction(
@@ -270,11 +267,6 @@ public class EditServerMVCActionCommand
 		sendRedirect(actionRequest, actionResponse, redirect);
 	}
 
-	@Override
-	public String getOSGiServiceIdentifier() {
-		return EditServerMVCActionCommand.class.getName();
-	}
-
 	private static void _resetLogLevels(
 		Map<String, String> logLevels, Map<String, String> customLogSettings) {
 
@@ -285,19 +277,32 @@ public class EditServerMVCActionCommand
 		}
 	}
 
-	private static void _updateLogLevels(
-		Map<String, String> logLevels, String osgiServiceIdentifier) {
+	private static void _updateLogLevels(Map<String, String> logLevels) {
+		for (Map.Entry<String, String> logLevelEntry : logLevels.entrySet()) {
+			Log4JUtil.setLevel(
+				logLevelEntry.getKey(), logLevelEntry.getValue(), true);
+		}
 
-		EditServerMVCActionCommand editServerMVCActionCommand =
-			(EditServerMVCActionCommand)
-				IdentifiableOSGiServiceUtil.getIdentifiableOSGiService(
-					osgiServiceIdentifier);
-
-		if (editServerMVCActionCommand == null) {
+		if (!_clusterExecutor.isEnabled()) {
 			return;
 		}
 
-		editServerMVCActionCommand._updateLogLevels(logLevels);
+		if (_clusterMasterExecutor.isMaster()) {
+			ClusterRequest clusterRequest =
+				ClusterRequest.createMulticastRequest(
+					new MethodHandler(
+						_resetLogLevelsMethodKey, Log4JUtil.getPriorities(),
+						Log4JUtil.getCustomLogSettings()),
+					true);
+
+			clusterRequest.setFireAndForget(true);
+
+			_clusterExecutor.execute(clusterRequest);
+		}
+		else {
+			_clusterMasterExecutor.executeOnMaster(
+				new MethodHandler(_updateLogLevelsMethodKey, logLevels));
+		}
 	}
 
 	private void _cacheDb() throws Exception {
@@ -710,36 +715,6 @@ public class EditServerMVCActionCommand
 		_updateLogLevels(logLevels);
 	}
 
-	private void _updateLogLevels(Map<String, String> logLevels) {
-		for (Map.Entry<String, String> logLevelEntry : logLevels.entrySet()) {
-			Log4JUtil.setLevel(
-				logLevelEntry.getKey(), logLevelEntry.getValue(), true);
-		}
-
-		if (!_clusterExecutor.isEnabled()) {
-			return;
-		}
-
-		if (_clusterMasterExecutor.isMaster()) {
-			ClusterRequest clusterRequest =
-				ClusterRequest.createMulticastRequest(
-					new MethodHandler(
-						_resetLogLevelsMethodKey, Log4JUtil.getPriorities(),
-						Log4JUtil.getCustomLogSettings()),
-					true);
-
-			clusterRequest.setFireAndForget(true);
-
-			_clusterExecutor.execute(clusterRequest);
-		}
-		else {
-			_clusterMasterExecutor.executeOnMaster(
-				new MethodHandler(
-					_updateLogLevelsMethodKey, logLevels,
-					getOSGiServiceIdentifier()));
-		}
-	}
-
 	private void _updateMail(
 			ActionRequest actionRequest, PortletPreferences portletPreferences)
 		throws Exception {
@@ -889,6 +864,12 @@ public class EditServerMVCActionCommand
 	private static final Log _log = LogFactoryUtil.getLog(
 		EditServerMVCActionCommand.class);
 
+	@Reference
+	private static ClusterExecutor _clusterExecutor;
+
+	@Reference
+	private static ClusterMasterExecutor _clusterMasterExecutor;
+
 	private static final Function<InvocationHandler, Layout>
 		_proxyProviderFunction = ProxyUtil.getProxyProviderFunction(
 			Layout.class, ModelWrapper.class);
@@ -896,17 +877,10 @@ public class EditServerMVCActionCommand
 		EditServerMVCActionCommand.class, "_resetLogLevels", Map.class,
 		Map.class);
 	private static final MethodKey _updateLogLevelsMethodKey = new MethodKey(
-		EditServerMVCActionCommand.class, "_updateLogLevels", Map.class,
-		String.class);
+		EditServerMVCActionCommand.class, "_updateLogLevels", Map.class);
 
 	@Reference
 	private AudioProcessor _audioProcessor;
-
-	@Reference
-	private ClusterExecutor _clusterExecutor;
-
-	@Reference
-	private ClusterMasterExecutor _clusterMasterExecutor;
 
 	@Reference(target = "(type=" + DLProcessorConstants.PDF_PROCESSOR + ")")
 	private DLProcessor _dlProcessor;
