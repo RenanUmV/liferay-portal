1c544be6951e55f18c4f9510497ecb7661d640e9
Renan Vasconelos <renan.vasconcelos@liferay.com>
2023-09-26 17:00:17 -0300

    LPS-197331 Remove service from DLPortletToolbarContributorHelper, rename it to DLPortletToolbarContributorUtil pass dlAppLocalService as paramater and apply to usages

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/toolbar/contributor/util/MenuItemProvider.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/toolbar/contributor/util/MenuItemProvider.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.document.library.web.internal.portlet.toolbar.contributor.helper;
+package com.liferay.document.library.web.internal.portlet.toolbar.contributor.util;
 
 import com.liferay.depot.group.provider.SiteConnectedGroupGroupProvider;
 import com.liferay.document.library.constants.DLPortletKeys;
