f31ef617215161e451096ba751e8a03872d89f65
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2023-11-14 10:27:44 -0300

    LPS-201309 Inline getSXPParameterContributorDefinitions from SXPParameterDataCreator into SXPParameterContributorDefinitionResourceImpl and remove SXPParameterContributorDefinitionProvider implementation from SXPParameterDataCreator

--- modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/SXPParameterContributorDefinitionResourceImpl.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/SXPParameterContributorDefinitionResourceImpl.java
@@ -6,14 +6,21 @@
 package com.liferay.search.experiences.rest.internal.resource.v1_0;
 
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.vulcan.pagination.Page;
-import com.liferay.search.experiences.blueprint.parameter.contributor.SXPParameterContributorDefinitionProvider;
+import com.liferay.search.experiences.rest.contributor.SXPParameterContributor;
+import com.liferay.search.experiences.rest.contributor.SXPParameterContributorRegistry;
 import com.liferay.search.experiences.rest.dto.v1_0.SXPParameterContributorDefinition;
 import com.liferay.search.experiences.rest.resource.v1_0.SXPParameterContributorDefinitionResource;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * @author Brian Wing Shun Chan
@@ -34,8 +41,7 @@ public class SXPParameterContributorDefinitionResourceImpl
 
 		return Page.of(
 			transform(
-				_sxpParameterContributorDefinitionProvider.
-					getSXPParameterContributorDefinitions(
+					_getSXPParameterContributorDefinitions(
 						contextCompany.getCompanyId(),
 						contextAcceptLanguage.getPreferredLocale()),
 				sxpParameterContributorDefinition ->
@@ -55,11 +61,36 @@ public class SXPParameterContributorDefinitionResourceImpl
 					}));
 	}
 
+	private List<com.liferay.search.experiences.blueprint.parameter.contributor.SXPParameterContributorDefinition>
+	_getSXPParameterContributorDefinitions(long companyId, Locale locale) {
+
+		if (ArrayUtil.isEmpty(
+			_sxpParameterContributorRegistry.
+				getSxpParameterContributors())) {
+
+			return Collections.emptyList();
+		}
+
+		List<com.liferay.search.experiences.blueprint.parameter.contributor.SXPParameterContributorDefinition>
+			sxpParameterContributorDefinitions = new ArrayList<>();
+
+		for (SXPParameterContributor sxpParameterContributor :
+			_sxpParameterContributorRegistry.
+				getSxpParameterContributors()) {
+
+			sxpParameterContributorDefinitions.addAll(
+				sxpParameterContributor.getSXPParameterContributorDefinitions(
+					companyId, locale));
+		}
+
+		return sxpParameterContributorDefinitions;
+	}
+
+	@Reference
+	private SXPParameterContributorRegistry _sxpParameterContributorRegistry;
+
 	@Reference
 	private Language _language;
 
-	@Reference
-	private SXPParameterContributorDefinitionProvider
-		_sxpParameterContributorDefinitionProvider;
 
 }
