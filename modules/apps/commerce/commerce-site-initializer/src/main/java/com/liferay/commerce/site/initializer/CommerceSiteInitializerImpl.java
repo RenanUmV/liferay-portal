df3f7dc0744b7d296513b885dd22d3b96e0a683f
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2024-01-03 14:49:11 -0300

    LPS-204949 Rename CPOptionCategoriesImporter to CPOptionCategoriesImporterUtil and make this a util class

--- modules/apps/commerce/commerce-site-initializer/src/main/java/com/liferay/commerce/site/initializer/CommerceSiteInitializerImpl.java
+++ modules/apps/commerce/commerce-site-initializer/src/main/java/com/liferay/commerce/site/initializer/CommerceSiteInitializerImpl.java
@@ -10,7 +10,7 @@ import com.liferay.commerce.configuration.CommerceAccountGroupServiceConfigurati
 import com.liferay.commerce.constants.CommerceConstants;
 import com.liferay.commerce.currency.service.CommerceCurrencyLocalService;
 import com.liferay.commerce.initializer.util.CPDefinitionsImporter;
-import com.liferay.commerce.initializer.util.CPOptionCategoriesImporter;
+import com.liferay.commerce.initializer.util.CPOptionCategoriesImporterUtil;
 import com.liferay.commerce.initializer.util.CPOptionsImporter;
 import com.liferay.commerce.initializer.util.CPSpecificationOptionsImporter;
 import com.liferay.commerce.initializer.util.CommerceInventoryWarehousesImporter;
@@ -911,7 +911,7 @@ public class CommerceSiteInitializerImpl implements CommerceSiteInitializer {
 
 		JSONArray jsonArray = _jsonFactory.createJSONArray(json);
 
-		_cpOptionCategoriesImporter.importCPOptionCategories(
+		CPOptionCategoriesImporterUtil.importCPOptionCategories(
 			jsonArray, serviceContext.getScopeGroupId(),
 			serviceContext.getUserId());
 	}
@@ -1052,9 +1052,6 @@ public class CommerceSiteInitializerImpl implements CommerceSiteInitializer {
 
 	@Reference
 	private CPMeasurementUnitLocalService _cpMeasurementUnitLocalService;
-
-	@Reference
-	private CPOptionCategoriesImporter _cpOptionCategoriesImporter;
 
 	@Reference
 	private CPOptionLocalService _cpOptionLocalService;
